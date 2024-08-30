8811dfce3d5a7eedc685eecb69ee7867221a6879
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-08-30 12:01:54 +0900

    stub out storage-exercise

--- storage-exercise/shared/src/androidMain/kotlin/com/jetbrains/workshop/Storage.android.kt
+++ storage-exercise/shared/src/androidMain/kotlin/com/jetbrains/workshop/Storage.android.kt
@@ -1,13 +1,1 @@
 package com.jetbrains.workshop
-
-actual fun persistString(key: String, value: String) {
-    val context = android.content.ContextWrapper(null)
-    val file = java.io.File(context.filesDir, key)
-    file.writeText(value)
-}
-
-actual fun restoreString(key: String): String {
-    val context = android.content.ContextWrapper(null)
-    val file = java.io.File(context.filesDir, key)
-    return if (file.exists()) file.readText() else ""
-}
--- storage-exercise/shared/src/jvmMain/kotlin/com/jetbrains/workshop/Storage.jvm.kt
+++ storage-exercise/shared/src/jvmMain/kotlin/com/jetbrains/workshop/Storage.jvm.kt
@@ -1,13 +1,1 @@
 package com.jetbrains.workshop
-
-import java.io.File
-
-actual fun persistString(key: String, value: String) {
-    File("$key.txt").writeText(value)
-}
-
-actual fun restoreString(key: String): String {
-    val file = File("$key.txt")
-    if(!file.exists()) return ""
-    return file.readText()
-}
