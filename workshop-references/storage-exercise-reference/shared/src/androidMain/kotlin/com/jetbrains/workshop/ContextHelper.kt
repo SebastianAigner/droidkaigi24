1003f929bef802b490d180113a7f59340431d1d1
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-09-09 10:15:57 +0900

    change to ContextHelper-based impl

---
+++ workshop-references/storage-exercise-reference/shared/src/androidMain/kotlin/com/jetbrains/workshop/ContextHelper.kt
@@ -0,0 +1,7 @@
+package com.jetbrains.workshop
+
+import android.content.Context
+
+object ContextHelper {
+    var currentContext: Context? = null
+}
--- workshop-references/storage-exercise-reference/shared/src/androidMain/kotlin/com/jetbrains/workshop/Storage.android.kt
+++ workshop-references/storage-exercise-reference/shared/src/androidMain/kotlin/com/jetbrains/workshop/Storage.android.kt
@@ -1,13 +1,16 @@
 package com.jetbrains.workshop
 
+import android.app.Application
+import android.content.Context
+
+val context get() = ContextHelper.currentContext!!
+
 actual fun persistString(key: String, value: String) {
-    val context = android.content.ContextWrapper(null)
     val file = java.io.File(context.filesDir, key)
     file.writeText(value)
 }
 
 actual fun restoreString(key: String): String {
-    val context = android.content.ContextWrapper(null)
     val file = java.io.File(context.filesDir, key)
     return if (file.exists()) file.readText() else ""
 }
